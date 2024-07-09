021811a5dc4432cde33fbf32c966abbca98ae98b
Matthew Ashcroft <matt.ashcroft03@gmail.com>
2024-07-09 10:58:47 +0100

    5pebblwwall

--- pages/secretimagepage.vue
+++ pages/secretimagepage.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
         <h1>This is where i host my images hiiiiiiiiiiiiiiiiii</h1>
-        <img src="../assets/images/5pebblewall.png" alt="A cute cat" />
+        <img src="../assets/images/5pebblewallbanner.png" alt="A cute cat" />
     </div>
 </template>
